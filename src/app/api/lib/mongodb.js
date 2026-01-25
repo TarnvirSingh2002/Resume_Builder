41a2218c3aacf0c9488b3f742ef7a1b5d2c94f8c
Tarn <singhtarn2002@gmail.com>
2026-01-25 10:26:26 +0530

    resolved mongoDB problem

--- src/app/api/lib/mongodb.js
+++ src/app/api/lib/mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require ("mongoose");
 
 const dbConnect = async() =>{
     try{
-        await mongoose.connect("mongodb://localhost:27017/resume");
+        await mongoose.connect(process.env.MONGO_DB);
         console.log("Connected successfully");
     }
     catch(err){
